276e536a10ce5f89b7d7ebba16a405cc570db42b
devon <dkeim01@gmail.com>
2022-05-10 19:06:24 -0400

    grammar fixes

--- apps.js
+++ apps.js
@@ -1,4 +1,3 @@
-console.log('apps.js is connected')
 function createParagraph() {
     var wordOne = document.getElementById("nounOne").value;
     var wordTwo = document.getElementById("fname").value;
@@ -16,7 +15,7 @@ function createParagraph() {
 
     console.log(wordOne,wordTwo,wordThree,wordFour,wordFive,wordSix,wordSeven,wordEight,wordNine,wordTen,wordEleven,wordTwelve,wordThirteen)
 
-    var paragraph = "<p>   Almost every community in America now has a bowling " + wordOne + " because bowling has become very " + wordTwo + " with young " + wordThree + " ." + " Many of the become very " + wordFour + " at the game. The object of the game is to roll a heavy bowling " + wordFive + " down the alley and knock down the " + wordSix + "pins which are at the other end. If you knock them down in one roll, it's called a/an " + wordSeven + " ." + "If it takes two rolls, it's called a/an " + wordEight + " ." + "Many alleys have automatic " + wordNine + " setters. Others hire " + wordTen + " who set the pins by " + wordEleven + " ." + "The most important thing to remember when bowling is to make sure you have a good grip on the " + wordTwelve + " or you're liable to drop it on your " + wordThirteen + " ." + "This hurts like anything! </p>"
+    var paragraph = "<p>   Almost every community in America now has a bowling " + wordOne + " because bowling has become very " + wordTwo + " with young " + wordThree + " ." + " Many of them become very " + wordFour + " at the game. The object of the game is to roll a heavy bowling " + wordFive + " down the alley and knock down the " + wordSix + " pins which are at the other end. If you knock them down in one roll, it's called a/an " + wordSeven + " ." + "If it takes two rolls, it's called a/an " + wordEight + " ." + "Many alleys have automatic " + wordNine + " setters. Others hire " + wordTen + " who set the pins by " + wordEleven + " ." + "The most important thing to remember when bowling is to make sure you have a good grip on the " + wordTwelve + " or you're liable to drop it on your " + wordThirteen + " ." + "This hurts like anything! </p>"
 console.log(paragraph);
 
 document.getElementById("answer").innerHTML = paragraph;
